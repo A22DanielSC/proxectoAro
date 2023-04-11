11a1901ae1744d5fd898d38fd547dba71ccf10de
A22DanielSC <A22DanielSC@a13eq32.sanclemente.local>
2023-04-11 21:24:41 +0200

    Javadoc completado.

--- src/main/java/circulo/Main.java
+++ src/main/java/circulo/Main.java
@@ -4,29 +4,38 @@ import java.text.DecimalFormat;
 
 /**
  * Pruebas de refactorización en NetBeans con la clase Aro
+ *
  * @author Daniel Sebio
  */
+public class Main {
 
-public class Main { 
     /**
-     * Main
+     * Constructor vacío Main
+     *
+     */
+    public Main() {
+    }
+
+    /**
+     * Método Main donde se estructura el funcionamiento principal del programa
+     *
      * @param args argumentos
      */
     public static void main(String[] args) {
-        Aro circulo = new Aro(37,43,2.5);
-        String saida =
-                "A coordenada X é "+circulo.getCoordenadaX()+
-                "\nA coordenada Y é "+circulo.getCoordenadaY()+
-                "\nO radio é "+circulo.getRadio();
+        Aro circulo = new Aro(37, 43, 2.5);
+        String saida
+                = "A coordenada X é " + circulo.getCoordenadaX()
+                + "\nA coordenada Y é " + circulo.getCoordenadaY()
+                + "\nO radio é " + circulo.getRadio();
         circulo.setCoordenadaX(35);
         circulo.setCoordenadaY(20);
         circulo.setRadio(4.2);
         DecimalFormat dousDixitos = new DecimalFormat("0.00");
-        saida+="\nO diámetro é "+dousDixitos.format(circulo.obterDiametro());
-        saida+="\nA circunferencia é "+dousDixitos.format(circulo.obterCircunferencia());
-        saida+="\nA área é "+dousDixitos.format(circulo.obterSuperficie());
+        saida += "\nO diámetro é " + dousDixitos.format(circulo.obterDiametro());
+        saida += "\nA circunferencia é " + dousDixitos.format(circulo.obterCircunferencia());
+        saida += "\nA área é " + dousDixitos.format(circulo.obterSuperficie());
         System.out.println(saida);
-        
+
         System.out.println(saida);
         System.exit(0);
     }
